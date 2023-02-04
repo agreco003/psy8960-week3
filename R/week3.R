69007de4bf77721c40aa6197cc030b5ba9ecd2a1
agreco <greco031@umn.edu>
2023-02-04 17:13:56 -0600

    Cleaned data by timestamp (>= July 2017) and attention check (q6 == 1)

--- R/week3.R
+++ R/week3.R
@@ -5,3 +5,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 raw_df <- read.csv(file ="../data/week3.csv")
 raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
+clean_df <- raw_df[raw_df$timeStart >= "2017-07-01 00:00:00 CET",]
+clean_df <- clean_df[clean_df$q6 == 1,]
