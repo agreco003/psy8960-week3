ef0922e752c795dccd75fca6fa879186bfb28a1b
agreco <greco031@umn.edu>
2023-02-04 16:50:35 -0600

    reformatted character vectors to Start and End Times to POSIXct

--- R/week3.R
+++ R/week3.R
@@ -3,3 +3,5 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import and Cleaning
 raw_df <- read.csv(file ="../data/week3.csv")
+raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
+raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
