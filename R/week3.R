85a76f35d1bb353814ff219120bff515a26b31d5
agreco <greco031@umn.edu>
2023-02-05 12:21:14 -0600

    All Analysis complete exxcept final for loop

--- R/week3.R
+++ R/week3.R
@@ -7,3 +7,14 @@ raw_df$timeStart <- as.POSIXct(raw_df$timeStart)
 raw_df$timeEnd <- as.POSIXct(raw_df$timeEnd)
 clean_df <- raw_df[raw_df$timeStart >= "2017-07-01 00:00:00 CET",]
 clean_df <- clean_df[clean_df$q6 == 1,]
+
+# Analysis
+clean_df$timeSpent <- difftime(clean_df$timeEnd, clean_df$timeStart, tz = "CET", units = c("secs"))
+hist(as.numeric(clean_df$timeSpent))
+frequency_tables_list <- lapply(clean_df[,5:14], table)
+frequency_tables_list <- lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
+frequency_tables_list
+for(i in frequency_tables_list) {
+  barplot(as.numeric(frequency_tables_list))
+}
